89d1f58409e2cb54234d4748533916dc594a00c3
unknown <patrice@0001.fr>
2024-11-20 14:33:44 +0100

    UI BTN COUNTER

--- src/components/Counter/Counter.jsx
+++ src/components/Counter/Counter.jsx
@@ -1,4 +1,4 @@
-import {useDispatch } from "react-redux";
+import { useDispatch } from "react-redux";
 import {
   increment,
   decrement,
@@ -10,11 +10,65 @@ export function Counter() {
   const dispatch = useDispatch();
   return (
     <div className="flex flex-col items-center dark:bg-gray-800 dark:text-white h-screen">
-        <Count/>
+      <Count />
       <div className="flex flex-row gap-3 mt-4">
-        <button onClick={() => dispatch(increment())} className="border-2 border-blue-500 px-4 py-2 rounded-md">Increment</button>
-        <button onClick={() => dispatch(decrement())} className="border-2 border-blue-500 px-4 py-2 rounded-md">Decrement</button>
-        <button onClick={() => dispatch(reinitialize())} className="border-2 border-blue-500 px-4 py-2 rounded-md">Reinitialize</button>
+        <button
+          onClick={() => dispatch(increment())}
+          className="border-2 border-green-500 px-4 py-2 rounded-md"
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke-width="1.5"
+            stroke="green"
+            class="size-6"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="M12 9v6m3-3H9m12 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
+            />
+          </svg>
+        </button>
+        <button
+          onClick={() => dispatch(decrement())}
+          className="border-2 border-red-500 px-4 py-2 rounded-md"
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke-width="1.5"
+            stroke="red"
+            class="size-6"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="M15 12H9m12 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
+            />
+          </svg>
+        </button>
+        <button
+          onClick={() => dispatch(reinitialize())}
+          className="border-2 border-yellow-500 px-4 py-2 rounded-md"
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke-width="1.5"
+            stroke="yellow"
+            class="size-6"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="M16.023 9.348h4.992v-.001M2.985 19.644v-4.992m0 0h4.992m-4.993 0 3.181 3.183a8.25 8.25 0 0 0 13.803-3.7M4.031 9.865a8.25 8.25 0 0 1 13.803-3.7l3.181 3.182m0-4.991v4.99"
+            />
+          </svg>
+        </button>
       </div>
     </div>
   );
